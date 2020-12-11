96459a5f4cfaec69b6971e3a77669210c00c7faf
twdo1 <twdo1@DESKTOP-FVO3PVE>
2020-12-11 17:35:06 -0500

    fixing sorting and searching methods

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
+	static ArrayList<Integer> expenses = new ArrayList<Integer>();
+    
+	
     public static void main(String[] args) {
         /*System.out.println("Hello World!");*/
         System.out.println("\n**************************************\n");
         System.out.println("\tWelcome to TheDesk \n");
         System.out.println("**************************************");
+        expenses.add(1000);
+        expenses.add(2300);
+        expenses.add(45000);
+        expenses.add(32000);
+        expenses.add(110);
         optionsSelection();
 
     }
@@ -25,18 +34,12 @@ public class Main {
             System.out.println(arr[i]);
             // display the all the Strings mentioned in the String array
         }
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
-        ArrayList<Integer> expenses = new ArrayList<Integer>();
-        expenses.add(1000);
-        expenses.add(2300);
-        expenses.add(45000);
-        expenses.add(32000);
-        expenses.add(110);
-        expenses.addAll(arrlist);
+        
+       
         System.out.println("\nEnter your choice:\t");
         Scanner sc = new Scanner(System.in);
         int  options =  sc.nextInt();
-        while(options != 6) {
+        
                 switch (options){
                     case 1:
                         System.out.println("Your saved expenses are listed below: \n");
@@ -48,7 +51,6 @@ public class Main {
                         int value = sc.nextInt();
                         expenses.add(value);
                         System.out.println("Your value is updated\n");
-                        expenses.addAll(arrlist);
                         System.out.println(expenses+"\n");
                         optionsSelection();
 
@@ -81,19 +83,31 @@ public class Main {
                         break;
                 }
             }
-        }
+        
 
     
     private static void closeApp() {
         System.out.println("Closing your application... \nThank you!");
             }
+    
     private static void searchExpenses(ArrayList<Integer> arrayList) {
-        int leng = arrayList.size();
+        Scanner sc = new Scanner(System.in);
+    	int leng = arrayList.size();
         System.out.println("Enter the expense you need to search:\t");
-        //Complete the method
+        int expense = sc.nextInt();
+        
+        for(int i : arrayList)
+        {
+        	if (i == expense)
+        	{
+        		
+        	}
+        }
+        
     }
+    
     private static void sortExpenses(ArrayList<Integer> arrayList) {
-        int arrlength =  arrayList.size();
-       //Complete the method. The expenses should be sorted in ascending order.
+        
+        Collections.sort(arrayList);
     }
 }
